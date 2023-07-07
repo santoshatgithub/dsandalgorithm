10c581aa82b4362db7dd2ccdbac50fa8e7349ca7
Santosh Kumar <kumar.santosh1984@yahoo.in>
2023-07-07 21:43:06 +0530

    hing initial commit

---
+++ src/main/java/com/santosh/dsandalgorithm/DsandalgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.santosh.dsandalgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsandalgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsandalgorithmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santosh/dsandalgorithm/DsandalgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosh.dsandalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsandalgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
